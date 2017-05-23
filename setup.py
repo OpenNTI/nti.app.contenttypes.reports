0d2864114727c58f45fc91f787b79c3355ab3c2d
austingraham731@gmail.com <austin.graham@AustinGraham.local>
2017-05-23 10:23:21 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import codecs
+
+VERSION = '0.0.0'
+
+entry_points = {
+	"z3c.autoinclude.plugin": [
+		'target = nti.app.products',
+	],
+}
+
+tests_require = [
+	'nti.app.testing'
+]
+
+setup(
+	name='nti.app.contenttypes.reports',
+	version=VERSION,
+	author='NextThought',
+	author_email='austin.graham@nextthought.com',
+	description="Report generation for courses",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='pyramid reportlab courses reporting',
+	classifiers=[
+		'Framework :: Pyramid',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: Implementation :: CPython'
+	],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	namespace_packages=['nti', 'nti.app', 'nti.app.contenttypes', 'nti.app.contenttypes.reports'],
+	install_requires=[
+		'setuptools',
+		'z3c.rml',
+		'z3c.macro',
+		'z3c.pagelet',
+		'z3c.template',
+		'zope.viewlet',
+		'zope.contentprovider',
+		'nti.app.pyramid_zope',
+		'nti.app.products.gradebook',
+		'nti.app.products.courseware'
+	],
+	extras_require={
+		'test': tests_require,
+	},
+	entry_points=entry_points
+)
