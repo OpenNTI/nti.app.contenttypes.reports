ddcb0fda9c452cdcaa6239002061362c79e58f50
Josh Zuech <josh.zuech@nextthought.com>
2018-06-21 22:23:25 -0500

    Remove base report functionality from courseware reports

--- src/nti/app/contenttypes/reports/interfaces.py
+++ src/nti/app/contenttypes/reports/interfaces.py
@@ -12,7 +12,34 @@ from __future__ import absolute_import
 
 from zope import interface
 
+from zope.viewlet.interfaces import IViewletManager
+
 from nti.appserver.workspaces.interfaces import IContainerCollection
+
+from nti.schema.field import TextLine
+
+
+class IPDFReportView(interface.Interface):
+    """
+    An interface that all the reporting views
+    that generate PDFs and work from the same set
+    of PDF templates are expected to implement.
+
+    In this way, we have a distinct way of registering :mod:`z3c.macro``
+    definitions.
+    """
+
+    filename = TextLine(title=u"The final portion of the file name, usually the view name",
+                        required=False,
+                        default=u"")
+
+    report_title = TextLine(title=u"The title of the report.")
+
+
+class IPDFReportHeaderManager(IViewletManager):
+    """
+    Viewlet manager for the headers of pdf reports.
+    """
 
 
 class IReportLinkProvider(interface.Interface):
--- src/nti/app/contenttypes/reports/utils.py
+++ src/nti/app/contenttypes/reports/utils.py
@@ -8,7 +8,14 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+import six
+import nameparser
+
+from functools import total_ordering
+
 from zope import component
+
+from zope.cachedescriptors.property import Lazy
 
 from nti.contenttypes.reports.interfaces import IReport
 from nti.contenttypes.reports.interfaces import IReportFilter
@@ -16,7 +23,15 @@ from nti.contenttypes.reports.interfaces import IReportFilter
 from nti.contenttypes.reports.reports import evaluate_permission
 from nti.contenttypes.reports.reports import evaluate_predicate
 
+from nti.coremetadata.interfaces import IUser
+
+from nti.dataserver.users.interfaces import IFriendlyNamed
+
+from nti.dataserver.users import User
+
 from nti.externalization.interfaces import StandardExternalFields
+
+from nti.schema.eqhash import EqHash
 
 LINKS = StandardExternalFields.LINKS
 
@@ -37,3 +52,55 @@ def get_visible_reports_for_context(context, user):
             and evaluate_permission(report, context, user):
             result.append(report)
     return result
+
+
+def _get_name_values( user, username ):
+    if isinstance(user, six.string_types):
+        user = User.get_user(user)
+
+    if user and IUser.providedBy( user ):
+        named_user = IFriendlyNamed(user)
+        display = named_user.realname or named_user.alias or named_user.username
+        # We may be given a username to override the actual username; use it.
+        username = username or user.username
+
+        if named_user.realname and '@' not in named_user.realname:
+            human_name = nameparser.HumanName(named_user.realname)
+            last = human_name.last or ''
+            first = human_name.first or ''
+        else:
+            first = last = ''
+    else:
+        # IF a community/sharing scope, show generic.
+        return u'System', u'System', u'', u''
+    return username, display, first, last
+
+
+@total_ordering
+@EqHash("sorting_key")
+class UserInfo(object):
+    """
+    Holds general student info. 'count' and 'perc' are optional values.
+    """
+
+    def __init__(self, user=None, username=None, display=None, count=None, perc=None):
+
+        if username and display:
+            self.username = username
+            self.display = display
+            self.last_name = ''
+            self.first_name = ''
+        else:
+            self.username, self.display, self.first_name, self.last_name = _get_name_values( user, username )
+
+        self.count = count
+        self.perc = perc
+
+    def __lt__(self, other):
+        return self.sorting_key.lower() < other.sorting_key.lower()
+
+    @Lazy
+    def sorting_key(self):
+        if self.last_name and self.first_name:
+            return self.last_name + ', ' + self.first_name
+        return self.last_name or self.username
